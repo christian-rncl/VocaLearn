a04e0860e5db62d0de151a1783f67592f4f7b3d4
Christian Roncal <cjl.roncal@gmail.com>
2018-04-08 06:20:25 -0400

    Commented out debug prints for compare strings

--- src/Components/QuizQuestion.js
+++ src/Components/QuizQuestion.js
@@ -23,8 +23,8 @@ function compare_strings(result, phrase)
     var result_punctuationless = compress(result).toUpperCase();
     var phrase_punctuationless = compress(phrase).toUpperCase();
 
-      console.log("result punctuationless: " + result_punctuationless);
-      console.log("phrase punctuationless: " + phrase_punctuationless);
+      // console.log("result punctuationless: " + result_punctuationless);
+      // console.log("phrase punctuationless: " + phrase_punctuationless);
       return (result_punctuationless === phrase_punctuationless);
 }
 
