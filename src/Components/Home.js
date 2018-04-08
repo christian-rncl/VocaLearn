e2abffb83c6984f8f6ec00eb0fc8cb881d901077
Shane Miller <shanekmiller44@gmail.com>
2018-04-08 04:15:02 -0400

    Fixed Question Counting and Home Page Summary

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,10 @@ export default class Home extends React.Component{
   render(){
     return(
       <div className='Home'>
-        <h1>Welcome, Home</h1>
+        <h1>Welcome!</h1>
+        <p>This website enables users to study with their voice. When you can't study with someone, this website
+        will be your partner. You add a set of flashcards and then answer each flashcard outloud. VocaLearn will
+        tell you if you were right and will give you the pronounciation.</p>
       </div>
     );
   }
